7e4bfe7bdb65d242bc13b4ab54867b3be3a8ffda
Marco Upia <markremembers@gmail.com>
2023-11-03 17:45:11 -0400

    learing scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+const leastFavoriteCustomer = "Bob the Builder"
+
+function upperCaseCustomerName() {
+    return customerName = "BOB";
+}
+
+
+function setBestCustomer() {
+    return bestCustomer = "not bob";
+    
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "Bob the destroyer"
+}
